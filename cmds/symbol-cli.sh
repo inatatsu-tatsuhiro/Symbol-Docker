a930e44a632028b538317b36f7c1efda6b3a36ae
inatatsu-tatsuhiro <tatsuhiro.0906@gmail.com>
2020-10-22 03:31:47 +0900

    :+1: docker runコマンドにrmフラグを付与

--- cmds/symbol-cli.sh
+++ cmds/symbol-cli.sh
@@ -2,5 +2,5 @@
 
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE:-$0}"); pwd)
 
-docker run -i -v $SCRIPT_DIR/../symbol-cli.config.json:/root/symbol-cli.config.json symbol-cli symbol-cli $@
+docker run --rm -i -v $SCRIPT_DIR/../symbol-cli.config.json:/root/symbol-cli.config.json symbol-cli symbol-cli $@
 
